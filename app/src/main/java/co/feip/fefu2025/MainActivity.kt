73096a72f507fcfaac3d7193a26cc029a0f08ed9
abbeswrld <149209944+abbeswrld@users.noreply.github.com>
2025-03-20 22:46:34 +1000

    lab 2

--- app/src/main/java/co/feip/fefu2025/MainActivity.kt
+++ app/src/main/java/co/feip/fefu2025/MainActivity.kt
@@ -1,75 +1,26 @@
 package co.feip.fefu2025
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
+
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
-import androidx.core.content.ContextCompat
+import co.feip.fefu2025.databinding.MainActivityBinding
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
-
-    private var count: Int = 0
-    private lateinit var counterTextView: TextView
-    private val internetReceiver = InternetReceiver()
+    private val binding: MainActivityBinding by lazy {
+        MainActivityBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.main_activity)
-
-        counterTextView = findViewById(R.id.counter).apply {
-            text = count.toString()
-            setOnClickListener { text = (++count).toString() }
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            val lang = LanguageLabel(this).apply {
+                name = Constants.languages.random()
+                color = -0x1000000 or Random.nextInt(0xFFFFFF)
+                percentage = Random.nextFloat() * 100
+            }
+            binding.layout.addView(lang)
         }
-
-        savedInstanceState?.let {
-            count = it.getInt("count")
-            counterTextView.text = count.toString()
-        }
-
-        registerInternetReceiver()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(internetReceiver)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("count", count)
-    }
-
-    private fun registerInternetReceiver() {
-        ContextCompat.registerReceiver(
-            this,
-            internetReceiver,
-            IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION),
-            ContextCompat.RECEIVER_EXPORTED
-        )
-    }
-}
-
-class InternetReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent?) {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val network = connectivityManager.activeNetwork
-        val capabilities = connectivityManager.getNetworkCapabilities(network)
-
-        val status = when {
-            network == null || capabilities == null -> "Internet is disabled"
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> "WiFi is enabled"
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> "Cellular is enabled"
-            else -> "Internet is disabled"
-        }
-
-        Log.d("InternetReceiver", status)
     }
 }
